6452df943727191333a501f715dc77b1e98b11e9
李仁兵 <ziyoudelaoge@163.com>
2015-12-05 16:51:29 +0800

    编辑README:二次封装

--- CacheFileDemo/CacheFileDemo/ViewController.m
+++ CacheFileDemo/CacheFileDemo/ViewController.m
@@ -27,12 +27,20 @@
     _filePathLabel.numberOfLines = 0;
     [self.view addSubview:_filePathLabel];
     
-    //缓存文件
+    //数据
     NSString * content = @"定风波 苏轼\n（三月七日沙湖道中遇雨。雨具先去，同行皆狼狈，余独不觉。已而遂晴，故作此 ）。\n莫听穿林打叶声，何妨吟啸且徐行。竹杖芒鞋轻胜马，谁怕？ 一蓑烟雨任平生。\n料峭春风吹酒醒，微冷，山头斜照却相迎。回首向来萧瑟处，归去，也无风雨也无晴。";
     FileCacheModel * fileCacheModel = [[FileCacheModel alloc] init];
     fileCacheModel.fileName = @"定风波.苏轼";
     fileCacheModel.fileData = [content dataUsingEncoding:NSUTF8StringEncoding];
     fileCacheModel.fileType = @"txt";
+    
+    
+    NSString * content2 = @"少年听雨歌楼上。红烛昏罗帐。壮年听雨客舟中。江阔云低、断雁叫西风。\n而今听雨僧庐下。鬓已星星也。悲欢离合总无情。一任阶前、点滴到天明。";
+    FileCacheModel * fileCacheModel2 = [[FileCacheModel alloc] init];
+    fileCacheModel2.fileName = @"虞美人·听雨";
+    fileCacheModel2.fileData = [content2 dataUsingEncoding:NSUTF8StringEncoding];
+    fileCacheModel2.fileType = @"txt";
+    
     
     FileCacheOperationManager * fileCacheOperationManager =[[FileCacheOperationManager alloc] initWithSearchPathModel:NXSearchPathModelCache rootFilePath:@"宋词.苏轼"];
     [fileCacheOperationManager cacheFileWith:fileCacheModel success:^(FileCacheOperation *operation, NSString *filePath) {
@@ -46,13 +54,6 @@
             _filePathLabel.text = errorInfo;
         });
     }];
-    
-    
-    NSString * content2 = @"少年听雨歌楼上。红烛昏罗帐。壮年听雨客舟中。江阔云低、断雁叫西风。\n而今听雨僧庐下。鬓已星星也。悲欢离合总无情。一任阶前、点滴到天明。";
-    FileCacheModel * fileCacheModel2 = [[FileCacheModel alloc] init];
-    fileCacheModel2.fileName = @"虞美人·听雨";
-    fileCacheModel2.fileData = [content2 dataUsingEncoding:NSUTF8StringEncoding];
-    fileCacheModel2.fileType = @"txt";
     
     
     //二次封装应用
--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 - `FileCacheOperation`
 - `FileCacheOperationManager`
 
-####建立**FileCacheModel**对象####
+####建立FileCacheModel对象####
 ```objective-c
 NSString * content = @"定风波 苏轼\n（三月七日沙湖道中遇雨。雨具先去，同行皆狼狈，余独不觉。已而遂晴，故作此 ）。\n莫听穿林打叶声，何妨吟啸且徐行。竹杖芒鞋轻胜马，谁怕？ 一蓑烟雨任平生。\n料峭春风吹酒醒，微冷，山头斜照却相迎。回首向来萧瑟处，归去，也无风雨也无晴。";
 FileCacheModel * fileCacheModel = [[FileCacheModel alloc] init];
@@ -20,7 +20,7 @@ fileCacheModel.fileType = @"txt";
 ```
 注：文件名、数据不能为空
 
-####建立**FileCacheOperationManager**对象####
+####建立FileCacheOperationManager对象####
 ```objective-c
 FileCacheOperationManager * fileCacheOperationManager =[[FileCacheOperationManager alloc] initWithSearchPathModel:NXSearchPathModelCache rootFilePath:@"宋词.苏轼"];
 [fileCacheOperationManager cacheFileWith:fileCacheModel success:^(FileCacheOperation *operation, NSString *filePath) {
@@ -36,5 +36,17 @@ FileCacheOperationManager * fileCacheOperationManager =[[FileCacheOperationManag
 }];
 ```
 注：可以进行二次封装，类似于AFNetworking;rootFilePath是存储目录路径，多层次包含用‘.’隔开，例如本例子中**‘宋词.苏轼’**转化为存储目录为**/宋词/苏轼**
+
+####二次封装说明####
+- `SongCiDataCacheConfiguration:来封装存储模式、根目录`
+- `SongCiCacheOperationManager:继承于FileCacheOperationManager，单例`
+注：这里用一个例子来说明二次封装  
+```objective-c
+[[SongCiCacheOperationManager sharedSongciCacheOperationManager] cacheFileWith:fileCacheModel success:^(FileCacheOperation *operation, NSString *filePath) {
+    NSLog(@"二次封装----%@",filePath);
+} failure:^(FileCacheOperation *operation, NSString *errorInfo) {
+    NSLog(@"二次封装----%@",errorInfo);
+}];
+```
 
 
